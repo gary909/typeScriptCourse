eaaa9fddf22e559e1cfcd468820bf5c80e03bd3e
gary909 <25279957+gary909@users.noreply.github.com>
2023-11-23 19:20:37 +0100

    code

--- basics/basics.js
+++ basics/basics.js
@@ -1,3 +1,4 @@
 console.log("Hello!");
 console.log("Goodbye!");
 console.log(Math.round(7.334532352));
+var myNum = 42;
--- basics/basics.ts
+++ basics/basics.ts
@@ -1,3 +1,5 @@
 console.log("Hello!");
 console.log("Goodbye!");
 console.log(Math.round(7.334532352));
+
+let myNum : number = 42;
